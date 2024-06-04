46d9152ef010cb9e9aadec5fe3a1f33c2bfd2181
Rodrigo Beltran Pinto <rodribpinto@gmail.com>
2024-06-04 20:15:28 -0300

    COMMIT

--- src/Apuesta.java
+++ src/Apuesta.java
@@ -1,34 +1,39 @@
 public class Apuesta {
     
-    public static void realizarApuestaPlena(int apuesta, int resultado, int monto, int cuenta) {
+    public static int realizarApuestaPlena(int apuesta, int resultado, int monto, int cuenta) {
         if (apuesta == resultado) {
             cuenta += monto * 35;
         } else {
             cuenta -= monto;
         }
+        return cuenta;
     }
 
-    public static void realizarApuestaParImpar(boolean esPar, int resultado, int monto, int cuenta) {
+    public static int realizarApuestaParImpar(boolean esPar, int resultado, int monto, int cuenta) {
         if (esPar == Ruleta.esPar(resultado)) {
             cuenta += monto;
         } else {
             cuenta -= monto;
         }
+        return cuenta;
     }
 
-    public static void realizarApuestaColor(boolean esRojo, int resultado, int monto, int cuenta) {
+    public static int realizarApuestaColor(boolean esRojo, int resultado, int monto, int cuenta) {
         if (esRojo == Ruleta.esRojo(resultado)) {
             cuenta += monto;
         } else {
             cuenta -= monto;
         }
+        return cuenta;
     }
 
-    public static void realizarApuestaFaltaPasa(boolean esFalta, int resultado, int monto, int cuenta) {
+    public static int realizarApuestaFaltaPasa(boolean esFalta, int resultado, int monto, int cuenta) {
         if (esFalta == Ruleta.esFalta(resultado)) {
             cuenta += monto;
         } else {
             cuenta -= monto;
         }
+        return cuenta;
     }
 }
+
